832a1ac7fdc1ddcaf4683514d0361229f247a828
jay-a-shah <jay.a.shah@simformsolutions.com>
2022-01-24 18:52:54 +0530

    TE5- T312 Swift Advance Final Commit

---
+++ Demo app/Demo app/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Demo app
+//
+//  Created by Jay Shah on 27/01/22.
+//
+
+import UIKit
+
+protocol TextDelegate {
+    func sendText(text: String)
+}
+
+class SecondViewController: UIViewController{
+    //MARK: - Outlet
+    @IBOutlet weak var txtfield: UITextField!
+    //MARK: - Variable
+    var delegate : TextDelegate?
+    //MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+//MARK: - Outlet Action
+extension SecondViewController{
+    @IBAction func goToAnotherView(_ sender: UIButton) {
+        delegate?.sendText(text: txtfield.text ?? "No Value Inserted")
+        dismiss(animated: true, completion: nil)
+    }
+}
